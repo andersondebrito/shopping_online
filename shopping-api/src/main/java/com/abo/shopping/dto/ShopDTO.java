062bf56c1188259bb51bc0cfaaeb683c44fa4eaf
andersondebrito <andersondebritto@gmail.com>
2021-04-24 23:12:44 +0100

    Shopping API

---
+++ shopping-api/src/main/java/com/abo/shopping/dto/ShopDTO.java
@@ -0,0 +1,59 @@
+package com.abo.shopping.dto;
+
+import com.abo.shopping.model.Shop;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+public class ShopDTO {
+
+    @NotBlank
+    private String userIdentifier;
+    @NotNull
+    private Float total;
+    @NotNull
+    private Date date;
+    @NotNull
+    private List<ItemDTO> items;
+
+    public String getUserIdentifier() {
+        return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+        this.userIdentifier = userIdentifier;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<ItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDTO> items) {
+        this.items = items;
+    }
+
+    public static ShopDTO convert(Shop shop) {
+        ShopDTO shopDTO = new ShopDTO();
+        shopDTO.setUserIdentifier(shop.getUserIdentifier());
+        shopDTO.setTotal(shop.getTotal());
+        return shopDTO;
+    }
+}
---
+++ shopping-api/src/main/java/com/abo/shopping/repository/ShopRepository.java
@@ -0,0 +1,19 @@
+package com.abo.shopping.repository;
+
+import com.abo.shopping.model.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    public List<Shop> findAllByUserIdentifier(
+            String userIdentifier);
+
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    List<Shop> findAllByDateGreaterThan(Date date);
+}
