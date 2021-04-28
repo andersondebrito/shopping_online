35e7d65be79dbc63b7d906a6d4359861458df123
andersondebrito <andersondebritto@gmail.com>
2021-04-28 22:14:46 +0100

    Dockerfiles created

--- shopping-api/src/main/java/com/abo/shopping/service/ProductService.java
+++ shopping-api/src/main/java/com/abo/shopping/service/ProductService.java
@@ -2,6 +2,7 @@ package com.abo.shopping.service;
 
 import com.abo.shoppingclient.dto.ProductDTO;
 import com.abo.shoppingclient.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -10,11 +11,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ProductService {
 
+    @Value("${PRODUCT_API_URL:http://localhost:8081/product/}")
+    private String productApiURL;
+
     public ProductDTO getProductByIdentifier(String productIdentifier) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String url =
-                    "http://localhost:8081/product/" + productIdentifier;
+            String url = productApiURL + productIdentifier;
             ResponseEntity<ProductDTO> response =
                     restTemplate.getForEntity(url, ProductDTO.class);
             return response.getBody();
--- shopping-api/src/main/java/com/abo/shopping/service/UserService.java
+++ shopping-api/src/main/java/com/abo/shopping/service/UserService.java
@@ -2,6 +2,7 @@ package com.abo.shopping.service;
 
 import com.abo.shoppingclient.dto.UserDTO;
 import com.abo.shoppingclient.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -11,11 +12,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class UserService {
 
+    @Value("${USER_API_URL:http://localhost:8080/user/identification/}")
+    private String userApiURL;
+
     public UserDTO getUserByIdentification(String identification, String key) {
         try {
             RestTemplate restTemplate = new RestTemplate();
             UriComponentsBuilder builder = UriComponentsBuilder
-                    .fromHttpUrl("http://localhost:8080/user/identification/" + "/user/identification/" + identification);
+                    .fromHttpUrl(userApiURL + identification);
             builder.queryParam("key", key);
             ResponseEntity<UserDTO> response = restTemplate
                     .getForEntity(builder.toUriString(), UserDTO.class);
